36d8110bafe2395f3e986c8375d0a89e3c20b5b5
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-18 21:06:20 +0000

    chore: bump manifest version to v0.2.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 game 'gta5'
 name 'prp_lib'
 author 'Paradox'
-version '0.2.2'
+version '0.2.3'
 repository 'https://github.com/hi-i-am-andrew/prp_lib'
 description 'A library of shared functions to utilise in other resources.'
 
